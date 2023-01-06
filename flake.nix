29c629683e3940f5daa3ffe4d040d11d25a4b9f9
Eric Crosson <eric.s.crosson@utexas.edu>
2023-01-06 17:26:24 -0600

    feat: add ci workflow

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "GitHub Actions for Rust projects";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              actionlint.enable = true;
+              alejandra.enable = true;
+              prettier.enable = true;
+            };
+          };
+        };
+      in {
+        devShell = pkgs.mkShell {
+          inherit (checks.pre-commit-check) shellHook;
+        };
+      }
+    );
+}
